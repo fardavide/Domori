9dce2292f4d2b7e1aac841dfa5bc681e2e42121e
Davide Farella <davide.farella@icloud.com>
2025-06-25 12:19:30 +0200

    refactor: Extract app information section into a separate view component in SettingsView - Improved code readability and maintainability

--- Domori/Views/SettingsView.swift
+++ Domori/Views/SettingsView.swift
@@ -1,29 +1,32 @@
 import SwiftUI
 
 struct SettingsView: View {
+  
+  var appInformationSection: some View {
+    Section("App Information") {
+      
+      HStack {
+        Text("Version")
+        Spacer()
+        Text(Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as! String)
+          .foregroundColor(.secondary)
+      }
+      
+      HStack {
+        Text("Build")
+        Spacer()
+        Text(Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion") as! String)
+          .foregroundColor(.secondary)
+      }
+    }
+  }
+  
   var body: some View {
     NavigationView {
       Form {
-        Section("App Information") {
-          HStack {
-            Text("Version")
-            Spacer()
-            Text(Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as! String)
-              .foregroundColor(.secondary)
-          }
-          
-          HStack {
-            Text("Build")
-            Spacer()
-            Text(Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion") as! String)
-              .foregroundColor(.secondary)
-          }
-        }
+        appInformationSection
       }
       .navigationTitle("Settings")
-#if os(iOS)
-      .navigationBarTitleDisplayMode(.large)
-#endif
     }
   }
 }
